705fe22728e15f0e586484b544839147fd955291
David Mandel <mandeldl@users.noreply.github.com>
2017-03-02 23:21:41 -0500

    change to oldestCat

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ colors.setTheme({
 
 fetchCats()
   .then(uniq) // NO DOUBLE CATS
-  .tap((cats) => console.log(`Cat information system accessed. ${cats.length} cats found. Beginning weighing process...`))
+  .tap((cats) => console.log(`Cat information system accessed. ${cats.length} cats found. Beginning age-guessing process...`))
   .map((url) => {
     return request.get(url)
       // SPCA sometimes returns 403s for some cats, ignore this.
@@ -48,13 +48,15 @@ fetchCats()
       .then((catPage) => {
         const $ = cheerio.load(catPage);
         const name = $(".field-name-title h1").text();
-        const weight = $(".field-name-field-animal-weight .field-item").text();
-        const lbs = Number(/(\d+)lbs\./.exec(weight)[1]);
-        const oz = /(\d+)oz\./.test(weight) ? Number(/(\d+)oz\./.exec(weight)[1]) : 0;
+        const age = $(".field-name-field-animal-age .field-item").text().trim();
+        // const weight = $(".field-name-field-animal-weight .field-item").text();
+        const years = Number(/(\d+)\D*(\d+)/.exec(age)[1]);
+        const months = Number(/(\d+)\D*(\d+)/.exec(age)[2]);
+        // const oz = /(\d+)oz\./.test(weight) ? Number(/(\d+)oz\./.exec(weight)[1]) : 0;
         const isFemale = $(".field-name-field-gender .field-item").text().trim() === "Female";
 
-        console.log("Weighing cat: %s", colors.green(name));
-        return {name, lbs, oz, isFemale, url}
+        console.log(`Guessing %s's age: ${years} years ${months} months`, colors.green(name));
+        return {name, years, months, isFemale, url}
       })
       // Null for cats that cannot be parsed.
       .catch(() => {});
@@ -62,13 +64,13 @@ fetchCats()
   // Filter out unparsable cats.
   .then(compact)
   .then((cats) => {
-    let fattestCat = {lbs: 0, oz: 0};
+    let oldestCat = {years: 0, months: 0};
     cats.forEach((cat) => {
-      if (cat.lbs > fattestCat.lbs || (cat.lbs === fattestCat.lbs && cat.oz > fattestCat.oz)) {
-        fattestCat = cat;
+      if (cat.years > oldestCat.years || (cat.years === oldestCat.years && cat.months > oldestCat.months)) {
+        oldestCat = cat;
       }
     });
-    console.log(`The fattest cat is ${colors.green.underline(fattestCat.name)}. ${(fattestCat.isFemale ? "She" : "He")} weighs ${fattestCat.lbs} lbs and ${fattestCat.oz} oz.`.output);
+    console.log(`The oldest cat is ${colors.green.underline(oldestCat.name)}. ${(oldestCat.isFemale ? "She" : "He")} is ${oldestCat.years} years and ${oldestCat.months} months old.`.output);
     setTimeout(() => console.log("Opening cat profile..."), 2000);
-    setTimeout(() => opener(fattestCat.url), 4000);
+    setTimeout(() => opener(oldestCat.url), 4000);
   });
